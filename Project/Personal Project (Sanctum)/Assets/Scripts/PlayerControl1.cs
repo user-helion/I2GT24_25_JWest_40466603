9d9ce852cc071dc871ed9fefd5fb463b8a1eeef2
Jenny <jwest11@qub.ac.uk>
2024-10-30 16:07:59 +0000

    health bar

--- Project/Personal Project (Sanctum)/Assets/Scripts/PlayerControl1.cs
+++ Project/Personal Project (Sanctum)/Assets/Scripts/PlayerControl1.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class PlayerControl1 : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 10.0f; //player movement
     public float turnSpeed;
     public float horizontalInput;
     public float forwardInput;
-    public Rigidbody playerRb;
+
+    public Rigidbody playerRb; // player jump with physics
     public float jumpForce = 8;
     public float gravityModifier;
     public bool isOnGround = true;
 
+    public int maxHealth = 100; // player health
+    public int currentHealth;
+    public HealthBar healthBar;
+
+
     // Start is called before the first frame update
     void Start()
     {
+      currentHealth = maxHealth; // game start health is at max
+      healthBar.SetMaxHealth(maxHealth);
+
        playerRb = GetComponent<Rigidbody>(); //reference physics of rigidbody
        Physics.gravity *= gravityModifier;
     }
@@ -34,9 +43,20 @@ public class PlayerControl1 : MonoBehaviour
       if (Input.GetKeyDown(KeyCode.Space) && isOnGround) //jump when space key pressed
       {
         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // jump force and immediate reaction
-        isOnGround = false;
+        isOnGround = false; //only jump if on the ground
       }
-  
+
+      if (Input.GetKeyDown(KeyCode.H))
+      {
+        TakeDamage(10);
+      }
+
+      void TakeDamage(int damage)
+      {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+      }
+
     }
 
     private void OnCollisionEnter(Collision collision){
