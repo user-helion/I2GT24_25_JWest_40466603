9647348c57784e0eed327eb3b678cfbab78e8bc2
Jenny <jwest11@qub.ac.uk>
2024-10-30 13:08:43 +0000

    jump

--- Project/Personal Project (Sanctum)/Assets/Scripts/PlayerControl1.cs
+++ Project/Personal Project (Sanctum)/Assets/Scripts/PlayerControl1.cs
@@ -2,29 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerControl1 : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 10.0f;
     public float turnSpeed;
     public float horizontalInput;
     public float forwardInput;
-    
+    public Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       playerRb = GetComponent<Rigidbody>();
+       playerRb.AddForce(Vector3.up * 200); 
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
       horizontalInput = Input.GetAxis("Horizontal");
       forwardInput = Input.GetAxis("Vertical");
-
-      //Move player 
+      
       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
       transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
-      //transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-
+  
     }
 }
