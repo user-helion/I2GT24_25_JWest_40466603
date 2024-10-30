a5a0995d17ffe86f86fdef995b904f353dfe615f
Jenny <jwest11@qub.ac.uk>
2024-10-30 14:57:50 +0000

    gravity jumping

--- Project/Personal Project (Sanctum)/Assets/Scripts/PlayerControl1.cs
+++ Project/Personal Project (Sanctum)/Assets/Scripts/PlayerControl1.cs
@@ -9,22 +9,37 @@ public class PlayerControl1 : MonoBehaviour
     public float horizontalInput;
     public float forwardInput;
     public Rigidbody playerRb;
+    public float jumpForce = 8;
+    public float gravityModifier;
+    public bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
-       playerRb = GetComponent<Rigidbody>();
-       playerRb.AddForce(Vector3.up * 200); 
+       playerRb = GetComponent<Rigidbody>(); //reference physics of rigidbody
+       Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-      horizontalInput = Input.GetAxis("Horizontal");
-      forwardInput = Input.GetAxis("Vertical");
+      horizontalInput = Input.GetAxis("Horizontal"); //left and right or AD keys
       
-      transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+      forwardInput = Input.GetAxis("Vertical"); // forward and back or WS keys
+      
+      transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
+      
       transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+      if (Input.GetKeyDown(KeyCode.Space) && isOnGround) //jump when space key pressed
+      {
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // jump force and immediate reaction
+        isOnGround = false;
+      }
   
     }
+
+    private void OnCollisionEnter(Collision collision){
+        isOnGround = true; }
+
 }
